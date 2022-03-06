bc6c69c96a7f1e2935d0658de4fc4506aa1d6abe
Jeilson Araujo <jeilsonaraujo@icloud.com>
2022-03-06 12:39:26 -0300

    finish aula 14

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = Vue.createApp({
             url: "http://google.com",
             books:
                 [
-                    { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', url: "https://www.google.com/search?q=name+of+the+wind" },
-                    { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', url: "https://www.google.com/search?q=the+way+of+kings" },
-                    { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', url: "https://www.google.com/search?q=the+final+empire" },
+                    { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', url: "https://www.google.com/search?q=name+of+the+wind", isFav: true },
+                    { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', url: "https://www.google.com/search?q=the+way+of+kings", isFav: false },
+                    { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', url: "https://www.google.com/search?q=the+final+empire", isFav: true },
                 ],
         }
     },
