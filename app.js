c5b7872134b4eb5dc5b0b547bc2a10c2bc76b020
Jeilson Araujo <jeilsonaraujo@icloud.com>
2022-03-06 11:20:52 -0300

    finish aula 13

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const app = Vue.createApp({
     data() {
         return {
             hideBooks: true,
-
+            url: "http://google.com",
             books:
                 [
-                    {
-                        title: "The final empire",
-                        author: "Jhon Sword",
-                    },
-                    {
-                        title: "The middle age of empire",
-                        author: "Jhon Smell",
-                    },
-                    {
-                        title: "The start of empire",
-                        author: "Elton Smell",
-                    },
+                    { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', url: "https://www.google.com/search?q=name+of+the+wind" },
+                    { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', url: "https://www.google.com/search?q=the+way+of+kings" },
+                    { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', url: "https://www.google.com/search?q=the+final+empire" },
                 ],
         }
     },
