674f4b9ef3d18a60392d2538d9326c95026e1271
Jeilson Araujo <jeilsonaraujo@icloud.com>
2022-03-06 07:05:36 -0300

    finish aula 8

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const app = Vue.createApp({
     // template: "<h1>This is my first template</h1>"
+    data() {
+        return {
+            title: "The final empire",
+            author: "Jhon Smell",
+            age: 43
+        }
+    }
 })
 
 app.mount('#app')
