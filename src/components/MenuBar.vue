704357a808b8353dc4ff05e38077d7d26ca99d6a
Jeilson Araujo <jeilsonaraujo@icloud.com>
2022-03-06 22:35:00 -0300

    feat: adding functions

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        @click="isOpen = !isOpen"
+        v-bind:class="{ 'is-active': isOpen }"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" v-bind:class="{ 'is-active': isOpen }">
+      <div class="navbar-end">
+        <router-link to="/" class="navbar-item is-tab">Home</router-link>
+        <router-link to="/about" class="navbar-item is-tab">About</router-link>
+        <router-link to="/else" class="navbar-item is-tab"
+          >Something Else</router-link
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      isOpen: false,
+    };
+  },
+};
+</script>
+<style>
+</style>
